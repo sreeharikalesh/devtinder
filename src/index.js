fdf135e0ad988d136d6982f246c06d09d2ea3106
sreehari personal <sreeharikalesh@gmail.com>
2025-07-23 13:46:06 +0530

    Initial-commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req,res)=>{
+    res.send("hello")
+})
+
+app.listen(3000, ()=>{
+    console.log("listening to port 3000");
+})
