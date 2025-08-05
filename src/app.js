e294e0d06d219f99445b154cbc803d50a14d7de5
sreehari personal <sreeharikalesh@gmail.com>
2025-08-05 15:49:55 +0530

    added express router, removed unwanted routes and refactored the whole codebase to use router

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
+const cookieParser = require('cookie-parser');
+
+const authRouter = require('./routes/auth');
+const profileRouter = require('./routes/profile');
+const requestRouter = require('./routes/requests');
+
+
+
+
+app.use(express.json())
+app.use(cookieParser())
+
+
+app.use('/',authRouter);
+app.use('/',profileRouter);
+app.use('/',requestRouter);
+
+connectDB()
+  .then(() => {
+    console.log("database connected successfully");
+    app.listen(3000, () => {
+      console.log("listening to port 3000");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
